487a58e19a4a2e3933039eb2fd429575d2879ee1
Chandrashekhar Patidar <chandrashekhar.patidar@yahoo.com>
2022-01-19 21:43:14 -0500

    Initial code changes.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# GraphQL_NodeJS
-NodeJS with GraphQL
+# Node.js GRAPHQL Example
+
+> An example of how to setup and run GraphQL
+
+## Quick Start
+
+``` bash
+# Install dependencies
+npm install express-graphql
+npm install graphql
+npm install nodemon
+
+# Serve on localhost:5000
+npm run devStart
+
+```
+
+## Run GRAPHQL Agent from browser
+
+``` bash
+http://localhost:5000/graphql
+
+```
---
+++ graphqlserver.js
@@ -0,0 +1,136 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull
+} = require('graphql')
+const app = express()
+
+const authors = [
+	{ id: 100, name: 'Peter Lynch' },
+	{ id: 200, name: 'Benjamin Graham' },
+	{ id: 300, name: 'Keith Lard' },
+  { id: 400, name: 'Mark Minervini' },
+  { id: 500, name: 'Robert T. Kiyosaki' },
+  { id: 600, name: 'Influential Individuals' }
+]
+
+const books = [
+	{ id: 100, name: 'One Up On Wall Street', authorId: 100 },
+	{ id: 200, name: 'The Intelligent Investor', authorId: 200 },
+	{ id: 300, name: 'Warren Buffett: 43 Lessons for Business & Life', authorId: 300 },
+	{ id: 400, name: 'Think & Trade Like a Champion', authorId: 400 },
+	{ id: 500, name: 'Rich Dad Poor Dad', authorId: 500 },
+	{ id: 600, name: 'Warren Buffett: The Life, Lessons & Rules For Success', authorId: 600 }
+]
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  description: 'Book written by an author',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: (book) => {
+        return authors.find(author => author.id === book.authorId)
+      }
+    }
+  })
+})
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  description: 'Books author',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter(book => book.authorId === author.id)
+      }
+    }
+  })
+})
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    book: {
+      type: BookType,
+      description: 'A Single Book',
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (parent, args) => books.find(book => book.id === args.id)
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      description: 'All Books',
+      resolve: () => books
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: 'All Authors',
+      resolve: () => authors
+    },
+    author: {
+      type: AuthorType,
+      description: 'A Single Author',
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (parent, args) => authors.find(author => author.id === args.id)
+    }
+  })
+})
+
+const RootMutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'Root Mutation',
+  fields: () => ({
+    addBook: {
+      type: BookType,
+      description: 'Book Addition',
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) }
+      },
+      resolve: (parent, args) => {
+        const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
+        books.push(book)
+        return book
+      }
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: 'Author Addition',
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) }
+      },
+      resolve: (parent, args) => {
+        const author = { id: authors.length + 1, name: args.name }
+        authors.push(author)
+        return author
+      }
+    }
+  })
+})
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+  mutation: RootMutationType
+})
+
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql: true
+}))
+app.listen(5000, () => console.log('Server Running on Port 5000'))
